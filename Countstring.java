321c1cf5737fcaceb8713a6011924f8c8487c70f
Vishal Singh <101355316+88visu@users.noreply.github.com>
2022-08-04 15:30:17 +0530

    Add files via upload

---
+++ Countstring.java
@@ -0,0 +1,19 @@
+
+public class Countstring {
+
+	public static void main(String[] args) {
+		 String str = "Vishal Singh";    
+	        int count = 0;    
+	        System.out.println("The entered string is: "+str);    
+	        //Count the characters in the string except space    
+	        for(int i = 0; i < str.length(); i++) 
+	        {    
+	            if(str.charAt(i) != ' ')    
+	                count++;    
+	        }                
+	        //Displays the total number of characters in the string    
+	        System.out.println("Total number of characters in the string: " + count);    
+	    }      
+	
+
+	}
